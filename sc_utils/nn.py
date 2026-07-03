c7cb985e8d6960524b69f307e9194915c45845fa
Ushio <ushiosc@protonmail.com>
2026-07-02 19:42:49 -0700

    Modified nn module

--- sc_utils/nn.py
+++ sc_utils/nn.py
@@ -17,8 +17,8 @@ class ModuleTools(nn.Module):
     """Module Tools
     Make sure to create a 'self.config' object with all the '__init__' arguments in it.
     """
+
     def print_parameters(self):
-        # === Parameter count ===
         total_params = sum(p.numel() for p in self.parameters())
         trainable_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
         print(f"{type(self).__name__}")
@@ -26,8 +26,7 @@ class ModuleTools(nn.Module):
         print(f"Trainable parameters: {trainable_params:,}")
         print(f"Model size ≈ {total_params * 4 / (1024**2):.1f} MB (in FP32)")
 
-
-    def save(self, path):
+    def save_checkpoint(self, path):
         path = pathlib.Path(path)
         path.mkdir(parents=True, exist_ok=True)
         
@@ -42,21 +41,7 @@ class ModuleTools(nn.Module):
         print(f"[{type(self).__name__} saved] {path}")
 
     @classmethod
-    def load(cls, path, map_location=None):
-        # config_path = pathlib.Path(path) / "config.json"
-        # model_path = pathlib.Path(path) / "model.pt"
-
-        # with open(config_path, "r") as f:
-        #     config = json.loads(f.read())
-        sd, config = cls.read_state_dict(path, map_location)
-
-        model = cls(**config)
-        model.load_state_dict(sd)
-        print(f"[Model {cls.__name__} Loaded] {path}")
-        return model
-        
-    @classmethod
-    def read_state_dict(cls, path, map_location=None):
+    def read_checkpoint_state_dict(cls, path, map_location=None):
         config_path = pathlib.Path(path) / "config.json"
         model_path = pathlib.Path(path) / "model.pt"
 
@@ -66,3 +51,12 @@ class ModuleTools(nn.Module):
         sd = torch.load(model_path, map_location=map_location)
         print(f"[Retreived {cls.__name__} State Dict] {path}")
         return sd, config     
+
+    @classmethod
+    def load_checkpoint(cls, path, map_location=None):
+        sd, config = cls.read_checkpoint_state_dict(path, map_location)
+
+        model = cls(**config)
+        model.load_state_dict(sd)
+        print(f"[Model {cls.__name__} Loaded] {path}")
+        return model
