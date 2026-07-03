8af87e28ae55024913884f90338b4089ffa2a040
Ushio <ushiosc@protonmail.com>
2026-07-02 18:35:21 -0700

    Modified 'setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ setup(
     name='sc_utils',         # Name of your module
     version='0.2',        # Version number
     py_modules=[
-    'create_autoencoder_data',
-    "processing",
+    'sc_utils.create_autoencoder_data',
+    "sc_utils.processing",
     # 'video_processing', 
     # "image_processing", 
     # "file_processing",
     # "audio_processing",
     #"data_processing_webui",
-    "parquet_to_lmdb",
-    "youtube_download",
-    "hf_datasets",
-    "create_frame_generator_data",
-    "create_key",
-    "sc_datasets",
-    "sc_numpy",
-    "sc_nn",
-    "create_lmdb",
-    "cuda_check"], # Module name (without .py)
+    "sc_utils.parquet_to_lmdb",
+    "sc_utils.youtube_download",
+    "sc_utils.hf_datasets",
+    "sc_utils.create_frame_generator_data",
+    "sc_utils.create_key",
+    "sc_utils.sc_datasets",
+    "sc_utils.sc_numpy",
+    "sc_utils.sc_nn",
+    "sc_utils.create_lmdb",
+    "sc_utils.cuda_check"], # Module name (without .py)
 )
